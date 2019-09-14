808b0476c513ccee7a60807ea7d3870c3c5fa515
SpecDI <vlad@herghelegiu.com>
2019-09-15 00:26:22 +0300

    Added support for backend server

--- NetSys/ServerSys/ServerSys/Server.h
+++ NetSys/ServerSys/ServerSys/Server.h
@@ -2,13 +2,20 @@
 
 #include <boost/asio.hpp>
 
+#include <string>
+
 namespace NetSys {
+	using tcp = boost::asio::ip::tcp;
+
 	class Server {
 	public:
 		explicit Server(unsigned int port);
 
 		int run() const;
 	protected:
+		std::string read_message(tcp::socket& socket) const;
+		void send_message(tcp::socket& socket, const std::string& message) const;
+
 		unsigned int m_portNumber;
 	};
 }
