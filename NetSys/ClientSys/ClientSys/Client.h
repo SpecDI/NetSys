e6caab2a877afdf9f2268f8503ca8aa25b8e1656
SpecDI <vlad@herghelegiu.com>
2019-09-15 00:38:06 +0300

    Added support for client side

--- NetSys/ClientSys/ClientSys/Client.h
+++ NetSys/ClientSys/ClientSys/Client.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "boost/asio.hpp"
+
 namespace NetSys {
+	using tcp = boost::asio::ip::tcp;
+
 	class Client {
 	public:
-		Client();
+		explicit Client(unsigned int port);
+
+		void run() const;
+	protected:
+		unsigned int m_portNumber;
 	};
 }
--- NetSys/ClientSys/src/Client.cpp
+++ NetSys/ClientSys/src/Client.cpp
@@ -1,10 +1,30 @@
 #include "Client.h"
 
+#include "boost/asio.hpp"
+
 #include <iostream>
 
 namespace NetSys {
-	Client::Client()
+	Client::Client(unsigned int port)
+		: m_portNumber(port)
 	{
-		std::cout << "Client created\n";
+		std::cout << "Client created and sending data over port " << m_portNumber << std::endl;
+	}
+
+	void Client::run() const
+	{
+		// Create I/O service handler
+		boost::asio::io_service io_service;
+		// Create socket to transmit/receive data
+		tcp::socket socket(io_service);
+
+		socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), m_portNumber));
+
+		std::string message = "Hello from Client\n";
+		boost::system::error_code error;
+		boost::asio::write(socket, boost::asio::buffer(message), error);
+
+		boost::asio::streambuf receive_buffer;
+		boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
 	}
 }
